0e280bc003071bb29630acdde860b58b3678b93d
Steve Womble <swomble@aol.com>
2020-06-18 20:30:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ attr_accessor :name, :grade, :id
       SET name = ?
       SET grade = ?
       WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
 end
