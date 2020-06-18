2f6f3d799a00c2e74157ab1679098616c72d100d
Steve Womble <swomble@aol.com>
2020-06-18 16:25:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,11 +42,12 @@ attr_accessor :name, :grade, :id
   end
   
   def self.new_from_db(row)
-    new_student = self.new
+    new_student = 
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade[2]
     new_student
+    self.new()
   end
 
 end
